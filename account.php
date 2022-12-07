30022c76ddf802fe245d15c902859da0c20760a1
babybleep <alkhatibhanine@gmail.com>
2022-12-07 15:53:44 +0200

    fixed search bar and added icon to account

--- account.php
+++ account.php
@@ -1,161 +1,159 @@
 <!DOCTYPE html>
 
 <html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width" , initial-scale="1.0" />
-    <title>Maida - My Chef Profile</title>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-      integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
 
-    <link href="Profile.css" rel="stylesheet" />
-    <link rel="stylesheet" href="search.css" />
-    <link rel="stylesheet" href="account.css" />
-    <link rel="stylesheet" href="navbar.css" />
-    <link rel="stylesheet" href="footer.css" />
-  </head>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width" , initial-scale="1.0" />
+  <title>Maida - My Chef Profile</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-  <body>
-      <!-- NavBar Start -->
-      <nav id="header">
-      
-      </nav>
-      <!-- NavBar End -->
+  <link href="Profile.css" rel="stylesheet" />
+  <link rel="stylesheet" href="search.css" />
+  <link rel="stylesheet" href="account.css" />
+  <link rel="stylesheet" href="navbar.css" />
+  <link rel="stylesheet" href="footer.css" />
+</head>
 
-    <!--Profile Container-->
-    <div class="maincontainer">
-      <div class="maintext">
-        <span class="profile">Chef's Profile</span>
-      </div>
-      <div class="mainfixed">
-        <div class="fixed">
-          <li>
-            <ul class="username">
-              <strong>Username:</strong>
-              <br />
-              <ul>
-                Hanine Al Khatib
-              </ul>
-            </ul>
-            <br />
+<body>
+  <!-- NavBar Start -->
+  <nav id="header">
 
-            <ul class="numberoforders">
-              <strong>Number of Incoming Orders:</strong>
-              <br />
-              <ul>
-                17
-              </ul>
-            </ul>
-            <br />
-            <ul class="email">
-              <strong>Email:</strong>
-              <br />
-              <ul>
-                hanine.alkhatib@gmail.com
-              </ul>
-            </ul>
-            <br />
-          </li>
-        </div>
-        <div class="fixed2">
-          <img class="imgprofile" src="images/profile.png" />
-        </div>
-        <br />
-      </div>
-      <div class="buttons">
-        <button class="bmydishes">Edit My Profile</button>
-        <button class="bmydishes">View My Incoming Orders</button>
-      </div>
+  </nav>
+  <!-- NavBar End -->
+  <!-- sticky icon start -->
+  <div class=" text-center iconstick">Number
+  </div>
+  <!--Profile Container-->
+  <div class="maincontainer">
+    <div class="maintext">
+      <span class="profile">Chef's Profile</span>
     </div>
-    <!--Profile Container End-->
+    <div class="mainfixed">
+      <div class="fixed">
+        <li>
+          <ul class="username">
+            <strong>Username:</strong>
+            <br />
+            <ul>
+              Hanine Al Khatib
+            </ul>
+          </ul>
+          <br />
 
-    <center>
-      <div class="container">
-        <div class="mydishesHeader">
-          <h2 class="dishesAvailable">My Dishes</h2>
-          <hr />
-        </div>
-        <div class="slider">
-          <center>
-            <div class="cardsList">
-              <div class="cardContainer">
-                <div class="card">
-                  <img src="images/kebbe.jpeg" alt="Kebbe Image" />
-                </div>
-                <div class="cardDescription">
-                  <div class="name">Kebbe niyye</div>
-                  <div class="price">350.000L.L.</div>
-                </div>
+          <ul class="numberoforders">
+            <strong>Number of Incoming Orders:</strong>
+            <br />
+            <ul>
+              17
+            </ul>
+          </ul>
+          <br />
+          <ul class="email">
+            <strong>Email:</strong>
+            <br />
+            <ul>
+              hanine.alkhatib@gmail.com
+            </ul>
+          </ul>
+          <br />
+        </li>
+      </div>
+      <div class="fixed2">
+        <img class="imgprofile" src="images/profile.png" />
+      </div>
+      <br />
+    </div>
+    <div class="buttons">
+      <button class="bmydishes">Edit My Profile</button>
+      <button class="bmydishes">View My Incoming Orders</button>
+    </div>
+  </div>
+  <!--Profile Container End-->
+
+  <center>
+    <div class="container">
+      <div class="mydishesHeader">
+        <h2 class="dishesAvailable">My Dishes</h2>
+        <hr />
+      </div>
+      <div class="slider">
+        <center>
+          <div class="cardsList">
+            <div class="cardContainer">
+              <div class="card">
+                <img src="images/kebbe.jpeg" alt="Kebbe Image" />
               </div>
-              <div class="cardContainer">
-                <div class="card">
-                  <img src="images/kebbe.jpeg" alt="Kebbe Image" />
-                </div>
-                <div class="cardDescription">
-                  <div class="name">Kebbe niyye</div>
-                  <div class="price">350.000L.L.</div>
-                </div>
-              </div>
-              <div class="cardContainer">
-                <div class="card">
-                  <img src="images/kebbe.jpeg" alt="Kebbe Image" />
-                </div>
-                <div class="cardDescription">
-                  <div class="name">Kebbe niyye</div>
-                  <div class="price">350.000L.L.</div>
-                </div>
-              </div>
-              <div class="cardContainer">
-                <div class="card">
-                  <img src="images/kebbe.jpeg" alt="Kebbe Image" />
-                </div>
-                <div class="cardDescription">
-                  <div class="name">Kebbe niyye</div>
-                  <div class="price">350.000L.L.</div>
-                </div>
-              </div>
-              <div class="cardContainer">
-                <div class="card">
-                  <img src="images/kebbe.jpeg" alt="Kebbe Image" />
-                </div>
-                <div class="cardDescription">
-                  <div class="name">Kebbe niyye</div>
-                  <div class="price">350.000L.L.</div>
-                </div>
+              <div class="cardDescription">
+                <div class="name">Kebbe niyye</div>
+                <div class="price">350.000L.L.</div>
               </div>
             </div>
-          </center>
-        </div>
+            <div class="cardContainer">
+              <div class="card">
+                <img src="images/kebbe.jpeg" alt="Kebbe Image" />
+              </div>
+              <div class="cardDescription">
+                <div class="name">Kebbe niyye</div>
+                <div class="price">350.000L.L.</div>
+              </div>
+            </div>
+            <div class="cardContainer">
+              <div class="card">
+                <img src="images/kebbe.jpeg" alt="Kebbe Image" />
+              </div>
+              <div class="cardDescription">
+                <div class="name">Kebbe niyye</div>
+                <div class="price">350.000L.L.</div>
+              </div>
+            </div>
+            <div class="cardContainer">
+              <div class="card">
+                <img src="images/kebbe.jpeg" alt="Kebbe Image" />
+              </div>
+              <div class="cardDescription">
+                <div class="name">Kebbe niyye</div>
+                <div class="price">350.000L.L.</div>
+              </div>
+            </div>
+            <div class="cardContainer">
+              <div class="card">
+                <img src="images/kebbe.jpeg" alt="Kebbe Image" />
+              </div>
+              <div class="cardDescription">
+                <div class="name">Kebbe niyye</div>
+                <div class="price">350.000L.L.</div>
+              </div>
+            </div>
+          </div>
+        </center>
       </div>
-    </center>
+    </div>
+  </center>
 
 
-      <!-- Footer Start -->
-    
-      <footer id="footer">
-        
-      </footer>
-  
-      <!-- Footer End -->
-  
-  
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-         <script type="text/javascript">
-          $(function(){
-            $("#header").load("navbar.php");
-          });
-        </script>
-        
-        <script type="text/javascript">
-          $(function(){
-            $("#footer").load("footer.php");
-          });
-        </script>
-    
-  </body>
+  <!-- Footer Start -->
+
+  <footer id="footer">
+
+  </footer>
+
+  <!-- Footer End -->
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+  <script type="text/javascript">
+    $(function() {
+      $("#header").load("navbar.php");
+    });
+  </script>
+
+  <script type="text/javascript">
+    $(function() {
+      $("#footer").load("footer.php");
+    });
+  </script>
+
+</body>
+
 </html>
--- search.php
+++ search.php
@@ -1,275 +1,262 @@
 <?php
-  session_start();
-  include_once('config.php');
-  // redirect to preauth.php if not logged in
-  if(!isset($_SESSION['username'])){
-    header('Location: preauth.php');
-  }
+session_start();
+include_once('config.php');
+// redirect to preauth.php if not logged in
+if (!isset($_SESSION['username'])) {
+  header('Location: preauth.php');
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-    />
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-      integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 
-    <link rel="stylesheet" href="search.css" />
-    <link rel="stylesheet" href="footer.css" />
-    <link rel="stylesheet" href="navbar.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <title>Maida - Search</title>
-  </head>
-  <body>
-    <!-- NavBar Start -->
-    <nav id="header">
-      <!-- Loaded the Header through JQuery -->
-    </nav>
-    <!-- NavBar End -->
-    <div class="container">
-      <div class="row searchSection">
-        <div class="header">
-          <h1 style="font-family: 'Poppins', sans-serif">Search</h1>
-          <p>
-            Browse through an extensive variety of local and foreign
-            deliciousness!
-          </p>
-        </div>
-        <div style="height:100px; text-align:center;">
+  <link rel="stylesheet" href="search.css" />
+  <link rel="stylesheet" href="footer.css" />
+  <link rel="stylesheet" href="navbar.css" />
+
+  <title>Maida - Search</title>
+</head>
+
+<body>
+  <!-- NavBar Start -->
+  <nav id="header">
+    <!-- Loaded the Header through JQuery -->
+  </nav>
+  <!-- NavBar End -->
+  <div class="container">
+    <div class="row searchSection">
+      <div class="header">
+        <h1 style="font-family: 'Poppins', sans-serif">Search</h1>
+        <p>
+          Browse through an extensive variety of local and foreign
+          deliciousness!
+        </p>
+      </div>
+      <div style="height:100px; text-align:center;">
         <div class="searchContainer">
-          <img
-            src="images/searchicon.png"
-            alt="Search Icon"
-            width="30px"
-            class="searchIcon"
-          />
-          <input
-            style="font-family: 'Poppins', sans-serif"
-            type="text"
-            class="searchBar"
-            id = "search"
-            placeholder="Search meals"
-          />
+          <img src="images/searchicon.png" alt="Search Icon" width="30px" class="searchIcon" />
+          <input style="font-family: 'Poppins', sans-serif" type="text" class="searchBar" id="search" placeholder="Search meals" />
           <br>
         </div>
         <br>
-        <ul style="height:100px;overflow-y:scroll;width:28.5rem;margin-top:-25px;border-bottom-left-radius:5px;border-bottom-right-radius:5px;" id="result">
+        <ul style="height:100px;width:28.5rem;margin-top:-25px;border-bottom-left-radius:5px;border-bottom-right-radius:5px;" id="result">
         </ul>
-        </div>
-        
-        
-        <span class=""
-          ><button class="sortnfilter">
-            Sort <i class="fa-solid fa-sort"></i>
-          </button>
-          <button class="sortnfilter">
-            Filter <i class="fa-solid fa-filter"></i></button
-        ></span>
       </div>
-      <div class="categoriesSection">
-        <div class="section">
-          <h2 class="listheader">Italian</h2>
-          <center>
-            <div class="cardsList">
 
-              <?php
-                // select all dishes with italian type
-                $sql = "SELECT * FROM dishes WHERE cuisine = 'Italian'";
-                $query = $db->prepare($sql);
-                $query->execute();
-                $result = $query->fetchAll(PDO::FETCH_OBJ);
 
-                // if result is not empty
+      <span class=""><button class="sortnfilter">
+          Sort <i class="fa-solid fa-sort"></i>
+        </button>
+        <button class="sortnfilter">
+          Filter <i class="fa-solid fa-filter"></i></button></span>
+    </div>
+    <div class="categoriesSection">
+      <div class="section">
+        <h2 class="listheader">Italian</h2>
+        <center>
+          <div class="cardsList">
 
-                if($query->rowCount() > 0){
-                  foreach($result as $row){
-                    echo " <a href=\"process_order.php?id=$row->id\">
+            <?php
+            // select all dishes with italian type
+            $sql = "SELECT * FROM dishes WHERE cuisine = 'Italian'";
+            $query = $db->prepare($sql);
+            $query->execute();
+            $result = $query->fetchAll(PDO::FETCH_OBJ);
+
+            // if result is not empty
+
+            if ($query->rowCount() > 0) {
+              foreach ($result as $row) {
+                echo " <a href=\"process_order.php?id=$row->id\">
                           <div class='cardContainer'>
                             <div class='card'>
-                              <img src='ImageUploads/".$row->image."' alt='Kebbe Image' />
+                              <img src='ImageUploads/" . $row->image . "' alt='Kebbe Image' />
                             </div>
                             <div class='cardDescription'>
-                              <div class='name'>".$row->name."</div>
-                              <div class='price'>".
-                              $row->price."L.L.</div>
+                              <div class='name'>" . $row->name . "</div>
+                              <div class='price'>" .
+                  $row->price . "L.L.</div>
                             </div>
                           </div>
                           </a>";
-                  }
-                }
+              }
+            }
 
-                
-              ?>
 
-            </div>
-          </center>
-        </div>
-        <div class="section">
-          <center><hr /></center>
-          <h2 class="listheader">Turkish</h2>
-          <center>
-            <div class="cardsList">
-                
+            ?>
+
+          </div>
+        </center>
+      </div>
+      <div class="section">
+        <center>
+          <hr />
+        </center>
+        <h2 class="listheader">Turkish</h2>
+        <center>
+          <div class="cardsList">
+
             <?php
-                // select all dishes with turkish type
-                $sql = "SELECT * FROM dishes WHERE cuisine = 'Turkish'";
-                $query = $db->prepare($sql);
-                $query->execute();
-                $result = $query->fetchAll(PDO::FETCH_OBJ);
+            // select all dishes with turkish type
+            $sql = "SELECT * FROM dishes WHERE cuisine = 'Turkish'";
+            $query = $db->prepare($sql);
+            $query->execute();
+            $result = $query->fetchAll(PDO::FETCH_OBJ);
 
-                // if result is not empty
+            // if result is not empty
 
-                if($query->rowCount() > 0){
-                  foreach($result as $row){
-                    echo "
+            if ($query->rowCount() > 0) {
+              foreach ($result as $row) {
+                echo "
                           <a href=\"process_order.php?id=$row->id\">
                             <div class='cardContainer'>
                               <div class='card'>
-                                <img src='ImageUploads/".$row->image."' alt='Kebbe Image' />
+                                <img src='ImageUploads/" . $row->image . "' alt='Kebbe Image' />
                               </div>
                               <div class='cardDescription'>
-                                <div class='name'>".$row->name."</div>
-                                <div class='price'>".
-                                $row->price."L.L.</div>
+                                <div class='name'>" . $row->name . "</div>
+                                <div class='price'>" .
+                  $row->price . "L.L.</div>
                               </div>
                             </div>
                           </a>
                           
                           ";
-                  }
-                }
+              }
+            }
 
-                
-              ?>
-            </div>
-          </center>
-        </div>
-        <div class="section">
-          <center><hr /></center>
-          <h2 class="listheader">Chinese</h2>
-          <center>
-            <div class="cardsList">
+
+            ?>
+          </div>
+        </center>
+      </div>
+      <div class="section">
+        <center>
+          <hr />
+        </center>
+        <h2 class="listheader">Chinese</h2>
+        <center>
+          <div class="cardsList">
             <?php
-                // select all dishes with chinese type
-                $sql = "SELECT * FROM dishes WHERE cuisine = 'Chinese'";
-                $query = $db->prepare($sql);
-                $query->execute();
-                $result = $query->fetchAll(PDO::FETCH_OBJ);
+            // select all dishes with chinese type
+            $sql = "SELECT * FROM dishes WHERE cuisine = 'Chinese'";
+            $query = $db->prepare($sql);
+            $query->execute();
+            $result = $query->fetchAll(PDO::FETCH_OBJ);
 
-                // if result is not empty
+            // if result is not empty
 
-                if($query->rowCount() > 0){
-                  foreach($result as $row){
-                    echo " <a href=\"process_order.php?id=$row->id\">
+            if ($query->rowCount() > 0) {
+              foreach ($result as $row) {
+                echo " <a href=\"process_order.php?id=$row->id\">
                           <div class='cardContainer'>
                             <div class='card'>
-                              <img src='ImageUploads/".$row->image."' alt='Kebbe Image' />
+                              <img src='ImageUploads/" . $row->image . "' alt='Kebbe Image' />
                             </div>
                             <div class='cardDescription'>
-                              <div class='name'>".$row->name."</div>
-                              <div class='price'>".
-                              $row->price."L.L.</div>
+                              <div class='name'>" . $row->name . "</div>
+                              <div class='price'>" .
+                  $row->price . "L.L.</div>
                             </div>
                           </div>
                           </a>";
-                  }
-                }
+              }
+            }
 
-                
 
-                
-              ?>
-            </div>
-          </center>
-        </div>
-        <div class="section">
-          <center><hr /></center>
-          <h2 class="listheader">Other</h2>
-          <center>
-            <div class="cardsList">
+
+
+            ?>
+          </div>
+        </center>
+      </div>
+      <div class="section">
+        <center>
+          <hr />
+        </center>
+        <h2 class="listheader">Other</h2>
+        <center>
+          <div class="cardsList">
             <?php
-                // select all dishes with italian type
-                $sql = "SELECT * FROM dishes WHERE cuisine = 'None'";
-                $query = $db->prepare($sql);
-                $query->execute();
-                $result = $query->fetchAll(PDO::FETCH_OBJ);
+            // select all dishes with italian type
+            $sql = "SELECT * FROM dishes WHERE cuisine = 'None'";
+            $query = $db->prepare($sql);
+            $query->execute();
+            $result = $query->fetchAll(PDO::FETCH_OBJ);
 
-                // if result is not empty
+            // if result is not empty
 
-                if($query->rowCount() > 0){
-                  foreach($result as $row){
-                    echo " <a href=\"process_order?id=$row->id.php\">
+            if ($query->rowCount() > 0) {
+              foreach ($result as $row) {
+                echo " <a href=\"process_order?id=$row->id.php\">
                           <div class='cardContainer'>
                             <div class='card'>
-                              <img src='ImageUploads/".$row->image."' alt='Kebbe Image' />
+                              <img src='ImageUploads/" . $row->image . "' alt='Kebbe Image' />
                             </div>
                             <div class='cardDescription'>
-                              <div class='name'>".$row->name."</div>
-                              <div class='price'>".
-                              $row->price."L.L.</div>
+                              <div class='name'>" . $row->name . "</div>
+                              <div class='price'>" .
+                  $row->price . "L.L.</div>
                             </div>
                           </div>
                           </a>";
-                  }
-                }
+              }
+            }
 
-                
-              ?>
-            </div>
-          </center>
-        </div>
+
+            ?>
+          </div>
+        </center>
       </div>
     </div>
-    <!-- Footer Start -->
-    <br>
-    <footer id="footer">
+  </div>
+  <!-- Footer Start -->
+  <br>
+  <footer id="footer">
 
-    </footer>
+  </footer>
 
-    <!-- include the jquery cdn -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <!-- add the jquery code for header and footer -->
-    <script>
-      $(function(){
-        $("#header").load("navbar.php");
-        $("#footer").load("footer.php");
-      });
-    </script>
+  <!-- include the jquery cdn -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <!-- add the jquery code for header and footer -->
+  <script>
+    $(function() {
+      $("#header").load("navbar.php");
+      $("#footer").load("footer.php");
+    });
+  </script>
 
-    <script>
-      $(document).ready(function(){
-        $('#search').keyup(function(){
-          var input = $(this).val();
-          if(input != ""){
-            $.ajax({
-            url:"process_search.php",
-            method:"POST",
-            data:{input:input},
-            success:function(data){
+  <script>
+    $(document).ready(function() {
+      $('#search').keyup(function() {
+        var input = $(this).val();
+        if (input != "") {
+          $.ajax({
+            url: "process_search.php",
+            method: "POST",
+            data: {
+              input: input
+            },
+            success: function(data) {
               $('#result').html(data);
             }
-            });
-          }else{
-            $('#result').html("");
-          }
-          })
-          }
-        )
-      
-    </script>
-    <!-- Footer End -->
-  </body>
+          });
+        } else {
+          $('#result').html("");
+        }
+      })
+    })
+  </script>
+  <!-- Footer End -->
+</body>
+
 </html>
