9792e615ba78e617fe81bc8d0e95bd46766c24eb
Jamil Awada <jamilawada2002@gmail.com>
2022-12-11 13:14:46 +0200

    implemented delete

--- editing.php
+++ editing.php
@@ -157,14 +157,18 @@ if (!isset($_SESSION['username'])) {
                     </br>
                     <div class="container-fluid">
                       <div class="mt-5 text-center">
-                        <button class="clear-btn">
+                        <div class="clear-btn">
                           <a href="account.php" style="text-decoration: none; color: inherit;">
                             Cancel
                           </a>
-                        </button>
+                        </div>
 
                         <button name='submit' class="save-btn" type="submit">Save Changes</button>
-                        <button name='delete' class="delete-btn" type="delete">Delete</button>
+                        <div name='delete' class="delete-btn">
+                          <a href="process_delete.php?dishid=<?php echo $id ?>" style="text-decoration: none; color: inherit;">
+                            Delete
+                          </a>
+                        </div>
                       </div>
                     </div>
                   </div>
